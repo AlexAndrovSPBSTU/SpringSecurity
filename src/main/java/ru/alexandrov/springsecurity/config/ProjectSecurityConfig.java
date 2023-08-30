3c04b0c9ba6a686602cb224c6775fbbe01d16b1e
Alexey Alexandrov <AleksandrovAlexey0929@yandex.ru>
2023-08-30 12:20:30 +0300

    add custom filters

--- src/main/java/ru/alexandrov/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/ru/alexandrov/springsecurity/config/ProjectSecurityConfig.java
@@ -14,7 +14,9 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import ru.alexandrov.springsecurity.filters.AuthoritiesLoggingAfterFilter;
 import ru.alexandrov.springsecurity.filters.CsrfCookieFilter;
+import ru.alexandrov.springsecurity.filters.RequestValidationBeforeFilter;
 
 import java.util.Collections;
 
@@ -40,22 +42,24 @@ public class ProjectSecurityConfig {
                         config.setMaxAge(3600L);
                         return config;
                     }
-                    }))
-                        .csrf((csrf) -> csrf
-                                .csrfTokenRequestHandler(requestHandler)
-                                .ignoringRequestMatchers("/contact", "/register")
-                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        )
-                        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                        .authorizeHttpRequests((requests)->requests
-                                .requestMatchers("/myAccount").hasRole("USER")
-                                .requestMatchers("/myBalance").hasAnyRole("USER","ADMIN")
-                                .requestMatchers("/myLoans").hasRole("USER")
-                                .requestMatchers("/myCards").hasRole("USER")
-                                .requestMatchers("/user").authenticated()
-                                .requestMatchers("/notices", "/contact", "/register").permitAll())
-                        .formLogin(Customizer.withDefaults())
-                        .httpBasic(Customizer.withDefaults());
+                }))
+                .csrf((csrf) -> csrf
+                        .csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/contact", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount").hasRole("USER")
+                        .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myCards").hasRole("USER")
+                        .requestMatchers("/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
